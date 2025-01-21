e57d1647a2e54e2eb684bc3e97ecaafe2e7ad3eb
Mario Ashraf <mario.ashraf@siliconexpert.com>
2025-01-21 18:31:00 +0200

    v1.0

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { type AddTask } from './task/task.model';
+import {TasksService} from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: false,
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name!: string;
+  isTaskFormVisible = false;
+
+  constructor(private tasksService: TasksService) {}
+  
+
+  get selectedUserTasks() {
+   return this.tasksService.getUserTasks(this.userId);
+  }
+
+  onCompleteTask(taskId: string) {
+    
+  }
+
+  onStartAddTask() {
+    this.isTaskFormVisible = true;
+  }
+
+  onCloseAddTask() {
+    this.isTaskFormVisible = false;
+  }
+
+
+
+}
